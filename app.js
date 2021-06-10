5aff6d99039792e4c1ed978e46425a119485dbda
SlackerEva <lesiv_anna@mail.ru>
2021-06-10 20:02:50 +0100

    change port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 dotenv.config();
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3001, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
