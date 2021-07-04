b33a874d0cc2a91ac004f2e57a2c34d8453e4249
SlackerEva <lesiv_anna@mail.ru>
2021-07-04 22:35:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const { errors } = require('celebrate');
 const routes = require('./routes/index');
